6bbea9041e801c66a5ac8a984110adefaa5e5546
Marrahy <sergimarenas@gmail.com>
2023-05-09 14:55:26 +0200

    Ej10_added

--- Excepciones_EJ10_UD7.java
+++ Excepciones_EJ10_UD7.java
@@ -1,3 +1,8 @@
+// Ejercicio 10
+// Implementa un programa que cree un vector tipo double de tamaño 5 y luego, utilizando un
+// bucle, pida cinco valores por teclado y los introduzca en el vector. Tendrás que manejar
+// la/las posibles excepciones y seguir pidiendo valores hasta rellenar el vector
+
 import java.util.Scanner;
 
 public class Excepciones_EJ10_UD7 {
@@ -5,5 +10,7 @@ public class Excepciones_EJ10_UD7 {
     private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         
+        
+
     }
 }
